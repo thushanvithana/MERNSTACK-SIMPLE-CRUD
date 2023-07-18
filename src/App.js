e5ade0d3b16bcf78633205baacb609788757b08c
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-07-18 18:09:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ItemList from './ItemList';
-import NavBar from './components/navBar';
-import SliderMenu from './components/slider';
+import React, { useEffect, useState, useRef  } from "react";
+import "./tesk.css";
 
-function App() {
+const CircleComponent = () => {
+  const [rotatedText, setRotatedText] = useState("");
+
+  useEffect(() => {
+    const originalText =  '      CONTACT-US-0772940951';          
+    const transformedText = originalText
+      .split("")
+      .map(
+        (char, i) =>
+        `<span style="transform:rotate(${90 + i * (360 / originalText.length)}deg)">${char}</span>`
+      )
+      .join("");
+    setRotatedText(transformedText);
+  }, []);
+
+  return (
+    <div className="circle">
+      <div className="logo">                         
+        <video
+          className="video"
+          src="../../video/test.mp4"
+          controls
+          autoPlay
+        ></video></div>
+      <div className="text">
+     
+        <p dangerouslySetInnerHTML={{ __html: rotatedText }} />
+      </div>
+    </div>
+  );
+};
+
+
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleTimeUpdate = () => {
+    // Update any progress bar or time display components if needed
+    // Example: Update current time and total duration
+    // const currentTime = videoRef.current.currentTime;
+    // const duration = videoRef.current.duration;
+  };
+
   return (
     <div>
-       <NavBar/>
-       <SliderMenu/>
-    <ItemList/>
-   
-  </div>
+      <video
+        ref={videoRef}
+        src="../../video/test.mp4"
+        onTimeUpdate={handleTimeUpdate}
+      />
+
+      <button onClick={handlePlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+    </div>
   );
-}
+};
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="vertical-container">
+        <div className="bottom-right">
+        
+          <CircleComponent />
+          
+        </div>
+  
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/ItemList.js
+++ src/ItemList.js
@@ -177,6 +177,7 @@ const ItemList = () => {
           </button>
         </div>
       )}
+      
     </div>
   );
 };
